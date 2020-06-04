765389fa9b53ebc11346a5bb28235290d6c46fcf
Satomi <satoskit@users.noreply.github.com>
2020-06-04 14:22:28 +0200

    add more details in home

--- src/contents/WelcomeText.vue
+++ src/contents/WelcomeText.vue
@@ -1,8 +1,31 @@
 <template>
 <article>
     <p>Welcome to About satoskit!</p>
-    <p>Satoskit is a software developer. 
-        Studied mainly Java at Java Enterprise Developer at Yrgo (higher vocational school) in Göteborg, Sweden.
+    <p>Satoskit is a software developer, currently located in Sweden. 
+        Studied mainly Java at Java Enterprise Developer program at Yrgo (higher vocational school) in Göteborg, Sweden.
     </p>
+    <h3>Key Tools/Technologies</h3>
+    <p v-for="tool in tools" :key="tool.name">{{tool.name}}</p>
 </article>
 </template>
+
+<script>
+export default {
+    name: 'WelcomeText',
+    data() {
+        return {
+            tools: [
+                { name: 'Java (EE, Spring)' },
+                { name: 'JavaScript (React.js, Vue.js, Node.js)' },
+                { name: 'SQL (MySQL, PostgreSQL)' },
+                { name: 'API (RESTful)' },
+                { name: 'Agile (Scrum, Kanban)' }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <welcome-text />
+        <WelcomeText />
     </div>
 </template>
 
