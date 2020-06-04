9aa165b2319343b649d7b74eab88f389b82c4e9a
Satomi <satoskit@users.noreply.github.com>
2020-06-04 14:07:23 +0200

    added if condition when japan on map is clicked

--- src/pages/Origin.vue
+++ src/pages/Origin.vue
@@ -1,9 +1,35 @@
 <template>
     <div>
         <h1>Origin!</h1>
+        <p>Find the origin of satoskit!</p>
         <img src="../assets/sekaichizu.png" alt="World Map" width="540" height="370" usemap="#worldmap" >
         <map name="worldmap">
-            <area shape="circle" coords="250, 113, 20" href="" alt="Japan!" title="Japan!">
+            <area shape="circle" coords="250, 113, 20" href="" alt="Japan!" title="Japan!" v-on:click="onclick">
         </map>
+        <div v-if="clicked">
+            <p>Japan!</p>
+            <p></p>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'Origin',
+    data() {
+        return {
+            clicked: false
+        }
+    },
+    methods: {
+        onclick(e) {
+            e.preventDefault()
+            this.clicked = true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
