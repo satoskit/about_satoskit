f4438d520c64e8bbf3b4100401c296a4614023ac
Satomi <satoskit@users.noreply.github.com>
2020-09-04 13:59:57 +0200

    added fontawesome icons

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faThumbsUp, faSeedling, faLeaf } from '@fortawesome/free-solid-svg-icons'
+import { faThumbsUp as faThumbsUpRegular } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faTrash)
+library.add(faTrash, faThumbsUp, faThumbsUpRegular, faSeedling, faLeaf)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
--- src/pages/CV.vue
+++ src/pages/CV.vue
@@ -1,13 +1,80 @@
 <template>
-  <h2>CV of Satomi Okuyama</h2>
+  <div id="cv">
+    <h2>CV of Satomi Okuyama</h2>
+    <div class="blocks">
+        <div class="block" id="keywords">
+            <h3>Keywords</h3>
+            <p v-for="keyword in keywords" :key="keyword">{{ keyword }}</p>
+        </div>
+        <div class="block">
+            <h3>Competences</h3>
+            <div class="inBlocks">
+                <div class="inBlock">
+                    <h4>Languages</h4>
+                    <div v-for="competence in competences" :key="competence">
+                    <p v-for="language in competence.languages" :key="language">
+                        {{language.name}}: <font-awesome-icon :icon="language.icon" /></p>
+                    </div>
+                </div>
+                <div class="inBlock">
+                    <h4>Databases</h4>
+                    <div v-for="competence in competences" :key="competence">
+                    <p v-for="database in competence.database" :key="database">
+                        {{database.name}}: <font-awesome-icon :icon="database.icon" /></p>
+                    </div>
+                </div>
+                <div class="inBlock">
+                    <h4>Others</h4>
+                    <div v-for="competence in competences" :key="competence">
+                    <p v-for="other in competence.others" :key="other">
+                        {{other.name}}: <font-awesome-icon :icon="other.icon" /></p>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
+    
+    
+  </div>
 </template>
 
 <script>
-export default {
+import { keywords } from '../contents/content-data'
+import { competences } from '../contents/content-data'
 
+export default {
+    data() {
+        return {
+            keywords: keywords,
+            competences: competences
+        }
+    },
+    methods: {
+        // not work
+        convertToIcons(n) {
+            switch (n) {
+                case 1: return `<font-awesome-icon :icon="['fas', 'thumbs-up']" />`
+                case 2: return <font-awesome-icon icon="leaf"></ font-awesome-icon> //`<font-awesome-icon :icon="['fas', 'thumbs-up']"></ font-awesome-icon>`
+            }
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.blocks, .inBlocks {
+    display: flex;
+    flex-wrap: wrap;
+}
+.block {
+    margin: 1rem;
+}
+.inBlock {
+    /* margin-right: 1rem; */
+    margin-left: 1rem;
+}
+#keywords > p {
+    margin-left: 0.5rem;
+}
 
-</style>script
+</style>
