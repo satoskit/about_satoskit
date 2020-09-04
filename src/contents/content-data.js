cb6611ba75a70caac75f1d2c6ed4325efb2715dc
Satomi <satoskit@users.noreply.github.com>
2020-09-04 13:58:32 +0200

    added more contents for CV

--- src/contents/content-data.js
+++ src/contents/content-data.js
@@ -1,12 +1,12 @@
 export const menus = [ 
+    { name: 'CV' },
     { name: 'Origin' }, 
     { name: 'Recipes' },
-    { name: 'CV' },
     { name: 'Contact' }
 ]
 
 export const tools = [
-    { name: 'Java (EE, Spring)' },
+    { name: 'Java (SE, EE, Spring)' },
     { name: 'JavaScript (React.js, Vue.js, Node.js)' },
     { name: 'SQL (MySQL, PostgreSQL)' },
     { name: 'API (RESTful)' },
@@ -14,3 +14,45 @@ export const tools = [
     { name: 'Agile (Scrum, Kanban)' },
 ]
 
+export const profile = [
+    { name: 'Satomi Okuyama' },
+    { languages: [
+        { language: 'Japanese', level: 'Native'},
+        { language: 'English', level: 'Fluent'},
+        { language: 'Swedish', level: 'Fluent'},
+        { language: 'Italian', level: 'Basic'}
+    ]}
+]
+
+export const keywords = [
+    'Programming, Java', 'Organized', 'Hard-worker', 'Curious', 'Flexible, open-minded',
+    'Service-minded', 'International', 'Public health'
+]
+
+export const competences = [
+    { languages: [ 
+        { name: 'Java (Spring)', level: 3, icon: ['fas', 'leaf'] },
+        { name: 'JavaScript (React.js, Vue.js)', level: 3, icon: ['fas', 'leaf']  },
+        { name: 'Bash', level: 2, icon: ['fas', 'seedling']  }
+    ]},
+    { database: [
+        { name: 'MySQL', level: 3, icon: ['fas', 'leaf'] },
+        { name: 'PostgreSQL', level: 3, icon: ['fas', 'leaf'] },
+        { name: 'SQLite', level: 2, icon: ['fas', 'seedling'] },
+    ]},
+    { OS: [
+        { name: 'iOS', level: 4 },
+        { name: 'Windows', level: 4 },
+        { name: 'Linux', level: 2, icon: ['fas', 'seedling'] }
+    ]},
+    { others: [
+        { name: 'Maven', level: 4, icon: ['fas', 'leaf'] },
+        { name: 'Git', level: 3, icon: ['fas', 'leaf'] },
+        { name: 'VirtualBox', level: 2, icon: ['fas', 'seedling'] },
+        { name: 'Grafana', level: 2, icon: ['fas', 'seedling'] },
+        { name: 'Docker', level: 3, icon: ['fas', 'leaf'] },
+        { name: 'AWS', level: 1, icon: ['fas', 'seedling'] },
+        { name: 'Heroku', level: 2, icon: ['fas', 'seedling'] },
+        { name: 'Scrum', level: 4, icon: ['fas', 'leaf'] }
+    ]}
+]
