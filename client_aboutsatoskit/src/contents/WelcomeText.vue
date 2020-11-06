af28da1b7366d2bcd58988066af2783ea8148176
Satomi <satoskit@users.noreply.github.com>
2020-11-06 14:39:26 +0100

    updated text in main page

--- client_aboutsatoskit/src/contents/WelcomeText.vue
+++ client_aboutsatoskit/src/contents/WelcomeText.vue
@@ -2,9 +2,10 @@
 <article>
     <p>Welcome to About satoskit!</p>
     <p>Satoskit is a software developer, currently located in Sweden. 
-        Studied mainly Java at Java Enterprise Developer program at Yrgo (higher vocational school) in Göteborg, Sweden.
+        Studied mainly Java at Java Enterprise Developer program at Yrgo in Göteborg, Sweden.
     </p>
-    <h3>Key Tools/Technologies</h3>
+    <p>This web application is a demo space to practice development skills.</p>
+    <h3>Key Tools/Technologies in this application</h3>
     <p v-for="tool in tools" :key="tool.name">{{tool.name}}</p>
 </article>
 </template>
