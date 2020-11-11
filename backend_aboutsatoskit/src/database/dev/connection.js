ae311f910c855a0f3f189826f5274fc7106729f9
Satomi <satoskit@users.noreply.github.com>
2020-11-11 15:45:08 +0100

    move docker-compose file to root directory, add .dockerignore, modify connection to database

--- backend_aboutsatoskit/src/database/dev/connection.js
+++ backend_aboutsatoskit/src/database/dev/connection.js
@@ -5,13 +5,6 @@ const pool = require('./pool')
 const connectionString = process.env.DATABASE_URL
 
 const client = new Client({connectionString, })
-// ({
-//     user: process.env.PGUSER,
-//     host: process.env.PGHOST,
-//     database: process.env.PGDATABASE,
-//     password: process.env.PGPASSWORD,
-//     port: process.env.PGPORT
-// })
 
 client.connect()
 
--- backend_aboutsatoskit/src/database/dev/demo-queries.js
+++ backend_aboutsatoskit/src/database/dev/demo-queries.js
@@ -3,10 +3,11 @@ const pool = require('./pool')
 
 const getAll = (req, res) => {
     pool.query('SELECT * FROM sampletable', (error, result) => {
-        if(error) {
-            throw error
+        try {
+            res.status(200).json(result.rows)
+        } catch (error) {
+            next(error)
         }
-        res.status(200).json(result.rows)
     })
 }
 
